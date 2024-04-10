d6e237ca729f50d0df24697416cc9676ae1ae06a
VLAD\micro <BenedettiV661@proton.me>
2024-04-10 16:49:43 +0300

    commit

--- src/main/kotlin/ru/fyberapptest/Application.kt
+++ src/main/kotlin/ru/fyberapptest/Application.kt
@@ -3,13 +3,11 @@ package ru.fyberapptest
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import ru.fyberapptest.plugins.*
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
-fun Application.module() {
-    configureRouting()
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()){
+        configureRouting()
+    }.start(wait = true)
 }
