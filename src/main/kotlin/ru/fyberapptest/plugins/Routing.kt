4ca23e1b80b8d83e9c001e4ea7fff2c340cb28ea
VLAD\micro <BenedettiV661@proton.me>
2024-04-12 16:38:49 +0300

    commit change DB 12

--- src/main/kotlin/ru/fyberapptest/plugins/Routing.kt
+++ src/main/kotlin/ru/fyberapptest/plugins/Routing.kt
@@ -291,7 +291,7 @@ fun Application.configureRouting(connection: Connection) {
 
                 // Send data to connected WebSocket clients
 
-                val listAllUsers:MutableList<User> = loadRepository.getAll()
+/*                val listAllUsers:MutableList<User> = loadRepository.getAll()
 
                 val json2: String = Json.encodeToString(listAllUsers)
 
@@ -299,13 +299,13 @@ fun Application.configureRouting(connection: Connection) {
 
                 connections.forEach { session ->
                     session.send(Frame.Text(json2))
-                }
+                }*/
 
                 val listTaskForUserId:MutableList<Task> = loadRepository.getTasksForUser(userId)
 
                 val json3: String = Json.encodeToString(listTaskForUserId)
 
-                println("$$$$$$$$$$$$$$$$$$$$$$$$ $json2")
+                println("$$$$$$$$$$$$$$$$$$$$$$$$ $json3")
 
                 connections.forEach { session ->
                     session.send(Frame.Text(json3))
